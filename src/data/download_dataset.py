e5b119f6399f645c3781762e46a61613c11010bd
Vanessa Timmer <v.timmer@student.tudelft.nl>
2024-05-02 01:23:16 +0200

    Added module docstring and changed folder_path to upper case for pylint

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -1,13 +1,15 @@
+"""This module downloads the dataset from kaggle and stores it in a folder."""
+
 import os
 import kaggle
 
 # Set the folder path
-folder_path = "data/raw"
+FOLDER_PATH = "data/raw"
 
 # Create the folder if it doesn't exist
-if not os.path.exists(folder_path):
+if not os.path.exists(FOLDER_PATH):
     os.chmod(".", 0o777)
-    os.makedirs(folder_path)
+    os.makedirs(FOLDER_PATH)
 
 # Download the dataset using the Kaggle API
-kaggle.api.dataset_download_files('aravindhannamalai/dl-dataset', path=folder_path, unzip=True)
+kaggle.api.dataset_download_files('aravindhannamalai/dl-dataset', path=FOLDER_PATH, unzip=True)
